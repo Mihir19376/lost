4baf1ef4aa793b4b0e51f6abbd1448b927fd8d85
Mihir19376 <19376@wc.school.nz>
2023-08-27 22:29:01 +1200

    commented player and boss scripts

--- Deep/Assets/Scripts/BossController.cs
+++ Deep/Assets/Scripts/BossController.cs
@@ -5,88 +5,124 @@ using UnityEngine.UI;
 
 public class BossController : MonoBehaviour
 {
+    // Indicates whether the boss level has been initiated.
     public bool bossLevelInitiated;
+    // The layer mask for detecting the player.
     public LayerMask playerLayer;
-
+    // Reference to the player's position.
     private Transform playerPos;
+    // The speed at which the boss moves.
     public float bossSpeed = .2f;
-
+    // Reference to the boss's animator.
     public Animator bossAnimator2D;
-
+    // The time left betweeen boss attacks.
     private float timeBetweenAttack;
+    // The initial time between boss attacks.
     private float startTimeBetweenAttack = .3f;
+    // Reference to the player object.
     public GameObject player;
-
+    // The health of the boss.
     public int bossHealth;
+    // Reference to the slider used to represent boss health.
     public Slider slider;
-
+    // Reference to the portal game object—used to win the game.
     public GameObject portal;
-
+    // Reference to a particle effect game object.
     public GameObject particleEffect;
+    // The maximum boss health
+    public int maxBossHealth = 30;
+    // The radius at which the boss checks for the player. 
+    private float checkRadius = .3f;
     // Start is called before the first frame update
     void Start()
     {
+        // denote that the boss level hast started yet when the game begins. 
         bossLevelInitiated = false;
+        // set the player object of the object in the scene that has the tag "playerTag"
         player = GameObject.FindGameObjectWithTag("playerTag");
-        bossHealth = 30;
+        // set the beggining boss health to 30.
+        bossHealth = maxBossHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Call a function to handle the health bar, updating the boss's health display.
         HandleHealthBar();
+        // Check if the boss level has been initiated (by the player entering through the barrier).
         if (bossLevelInitiated)
         {
-            // follow player
-            if (Physics2D.OverlapCircle(transform.position, .3f, playerLayer))
+            // Check if the boss is within a certain radius of the player using Physics2D.OverlapCircle.
+            if (Physics2D.OverlapCircle(transform.position, checkRadius, playerLayer))
             {
-                // attack player
+                // Set the boss's animator parameters to indicate the attack state.
                 bossAnimator2D.SetBool("bossAttacking", true);
                 bossAnimator2D.SetBool("bossRunning", false);
+
+                // Check if it's time for the boss to attack based on the timeBetweenAttack.
                 if (timeBetweenAttack <= 0)
                 {
+                    // Deal damage to the player by calling the TakePlayerDamage method on the player object.
                     player.GetComponent<PlayerMovement2D>().TakePlayerDamage(1);
+                    // Reset the timeBetweenAttack to the initial value.
                     timeBetweenAttack = startTimeBetweenAttack;
                 }
                 else
                 {
+                    // Decrease the timeBetweenAttack timer by deltaTime to control the attack rate.
                     timeBetweenAttack -= Time.deltaTime;
                 }
             }
             else
             {
-                // move towarsa plasy
+                // Move towards the player.
+                // Find the player's position based on the object in the scenw with the "playerTag" tag.
                 playerPos = GameObject.FindGameObjectWithTag("playerTag").GetComponent<Transform>();
+                // Calculate the target position, maintaining the same Y-coordinate as the boss.
                 Vector2 target = new Vector2(playerPos.position.x, transform.position.y);
+                // Move the boss towards the target position using MoveTowards.
                 transform.position = Vector2.MoveTowards(transform.position, target, bossSpeed * Time.deltaTime);
+                // Set animator parameters to indicate that the boss is back to moving.
                 bossAnimator2D.SetBool("bossAttacking", false);
                 bossAnimator2D.SetBool("bossRunning", true);
             }
 
+            // Call a function to make the boss character face towards the player.
             faceTowardsPlayer();
 
+            // Check if the boss's health has reached or fallen below zero.
             if (bossHealth <= 0)
             {
+                // Activate the portal game object so that te player can reach it and complete the game. 
                 portal.SetActive(true);
+                // Instantiate a particle effect at the boss's current position with no rotation.
                 Instantiate(particleEffect, transform.position, Quaternion.identity);
+                // Destroy the boss game object to remove it from the scene.
                 Destroy(gameObject);
             }
         }
     }
 
+    /// <summary>
+    /// Make the boss character face towards the player.
+    /// </summary>
     void faceTowardsPlayer()
     {
+        // Get the current scale of the boss character.
         Vector3 faceTowardsPlayerScale = transform.localScale;
 
+        // Check the relative position of the player and boss to determine the direction.
         if (playerPos.position.x >= transform.position.x)
         {
+            // If the player is to the right, set the X scale to its absolute value (positive).
             faceTowardsPlayerScale.x = Mathf.Abs(faceTowardsPlayerScale.x);
         }
         else
         {
+            // If the player is to the left, set the X scale to its absolute value (positive) and invert it (negative).
             faceTowardsPlayerScale.x = Mathf.Abs(faceTowardsPlayerScale.x) * -1;
         }
-
+        // Update the boss character's local scale to face towards the player.
         transform.localScale = faceTowardsPlayerScale;
     }
 
@@ -96,8 +132,13 @@ public class BossController : MonoBehaviour
         Gizmos.DrawWireSphere(transform.position, .3f);
     }
 
+    /// <summary>
+    /// Inflict damage on the boss character.
+    /// </summary>
+    /// <param name="damage">The amount of damage to deal to the boss.</param>
     public void TakeDamage(int damage)
     {
+        // Subtract the specified damage from the boss's health.
         bossHealth -= damage;
     }
 
--- Deep/Assets/Scripts/PlayerMovement2D.cs
+++ Deep/Assets/Scripts/PlayerMovement2D.cs
@@ -5,83 +5,118 @@ using UnityEngine.SceneManagement;
 
 public class PlayerMovement2D : MonoBehaviour
 {
+    // this is a reference to the Brackeys character controller C# asset
     public CharacterController2D controller2D;
+    // this is the run speed of the the character. 
     public float runSpeed = 40f;
-
+    // this is where I store the horizontal movement input of the player.
     private float horizontalMove = 0f;
+    // this is a boolean to denote if the character is jumping or not
     private bool jump = false;
-
+    // this is a reference to the characters animator
     public Animator animator2D;
-
+    // this is the axe that spawns with the player to attack/min.
     public GameObject axePrefab;
+    // this is a reference to the spawn positon of it (its the transform of an empty game object.)
     public Transform spawnPos;
+    // this int (-1 or 1) is multiplied to the spanwed axe's scale to make sure its facing the right way.
     int axeDirectionMultiplier;
+    // this denotes whether we are attacking or not
     bool isAttacking = false;
-
+    // this is the curent player's health. 
     public float currentPlayerHealth;
+    // this is the beggnign health. 
     public float maxPlayerHealth = 10;
-
-
+    // this is the number of gems the player has collected.
     public int gems;
-
+    // this is the rising heartbeat in the background sound effect.
     public AudioSource heartBeatAudio;
+    // this is the inital distacnce to the boss level based on the playes spawn pos.
     float initialDistanceToBossLevel;
+    // this is the object used to calculate that distance.
     public GameObject bossBarrier;
-
-
+    // this is a reference to the boss. 
     public GameObject boss;
     // Start is called before the first frame update
     void Start()
     {
+        // Set the volume of the heart beat to 0
         heartBeatAudio.volume = 0;
+        // Calculate the initial distance to the boss level by measuring the distance between
+        // the position of the 'bossBarrier' and the current object's position
         initialDistanceToBossLevel = Vector3.Distance(bossBarrier.transform.position, transform.position);
+        // Set the current player health to its maximum value.
         currentPlayerHealth = maxPlayerHealth;
+        // Initialize the 'gems' variable to 0, indicating that the player has collected no gems yet
         gems = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Call a function to manage audio.
         ManageAudio();
-        
+
+        // Get the horizontal input for player movement and multiply it by the runSpeed.
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-        Debug.Log("Horizontal Move: " + horizontalMove);
+
+        // Update the animator's "Speed" parameter with the absolute value of horizontalMove,
+        // for controlling the characters run anim based on movement speed.
         animator2D.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
+        // Check if the player pressed the "Jump" button.
         if (Input.GetButtonDown("Jump"))
         {
+            // Set the 'jump' flag to true, indicating the player wants to jump.
             jump = true;
+            // Update the animator to indicate that the character is jumping.
             animator2D.SetBool("IsJumping", true);
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftShift) && !isAttacking)
+        // Check if the player pressed the "Fire1" button and is not already attacking.
+        if (Input.GetButtonDown("Fire1") && !isAttacking)
         {
+            // Start a coroutine to spawn an axe.
             StartCoroutine(SpawnAxe());
         }
 
-        // if player health is below or equal to 0, die :)
-        if(currentPlayerHealth <= 0)
+        // Check if the player's health is below or equal to 0
+        if (currentPlayerHealth <= 0)
         {
-            //die
-            //Die();
+            // Start a coroutine to handle the player's death
             StartCoroutine(Die());
         }
     }
 
+    /// <summary>
+    /// when the character lands. this function is called externally. 
+    /// </summary>
     public void OnLanding()
     {
-        Debug.Log("Mon");
+        // Update the animator to indicate that the character is no longer jumping.
         animator2D.SetBool("IsJumping", false);
     }
 
+    // This method is called during the FixedUpdate physics update.
     private void FixedUpdate()
     {
+        // Use the 'controller2D' to move the character horizontally based on 'horizontalMove'.
+        // 'Time.fixedDeltaTime' is used to make the movement frame rate independent.
+        // 'jump' is just letting the controller know whether or not the character has jumped.
+        // the second parameter is for crouching. I don't use that. 
         controller2D.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+
+        // Reset the 'jump' flag after the character has potentially jumped.
         jump = false;
     }
 
+    /// <summary>
+    /// Coroutine to spawn an axe as a weapon.
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator SpawnAxe()
     {
+        // Determine the direction of the axe based on the character's local scale.
         if (transform.localScale.x < 0)
         {
             axeDirectionMultiplier = -1;
@@ -90,57 +125,96 @@ public class PlayerMovement2D : MonoBehaviour
         {
             axeDirectionMultiplier = 1;
         }
-        GameObject newObject = Instantiate(axePrefab, spawnPos.position, Quaternion.identity) as GameObject;  // instatiate the object
+        // Instantiate the axe object at the spawn position with no rotation.
+        GameObject newObject = Instantiate(axePrefab, spawnPos.position, Quaternion.identity) as GameObject;
+        // change the x scale to match up with the multiplier. 
         newObject.transform.localScale  = new Vector3(newObject.transform.localScale.x * axeDirectionMultiplier, newObject.transform.localScale.y, newObject.transform.localScale.z); // change its local scale in x y z format
+        // Set the 'isAttacking' flag to true to indicate that the character is currently attacking.
         isAttacking = true;
+        // Get the animator component of the spawned axe.
         Animator axeAnimator = newObject.GetComponent<Animator>();
+        // Wait for a duration equal to the length of the axe's animation.
         yield return new WaitForSeconds(axeAnimator.GetCurrentAnimatorStateInfo(0).length + axeAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime);
+        // Set the 'isAttacking' flag to false to indicate that the attack animation is complete.
         isAttacking = false;
     }
 
+    /// <summary>
+    /// Inflict damage on the player character.
+    /// </summary>
+    /// <param name="damageToDeal">The amount of damage to deal to the player.</param>
     public void TakePlayerDamage(int damageToDeal)
     {
+        // Subtract the specified damage from the player's current health.
         currentPlayerHealth -= damageToDeal;
     }
 
+    /// <summary>
+    /// Coroutine to handle the player character's death.
+    /// </summary>
+    /// <returns></returns>
     IEnumerator Die()
     {
-        //die
+        // Trigger the death animation by setting the "isDead" parameter in the animator to true.
         animator2D.SetBool("isDead", true);
+        // Wait for a duration equal to the length of the death animation.
         yield return new WaitForSeconds(animator2D.GetCurrentAnimatorStateInfo(0).length);
+        // Load the lose screen.
         SceneManager.LoadScene(2);
+        // Output a debugging message indicating that the player has died.
         Debug.Log("Player Died :)");
-
     }
 
+    /// <summary>
+    /// Increment the number of collected gems by one.
+    /// </summary>
     public void CollectGem()
     {
+        // Increase the count of collected gems by one.
         gems += 1;
     }
 
+    /// <summary>
+    /// Manages audio based on the player's distance to the boss level.
+    /// </summary>
     void ManageAudio()
     {
+        // Check if 'bossBarrier' is not null to avoid null reference errors.
         if (bossBarrier != null)
         {
+            // Calculate the distance between the player and the boss barrier.
             float distanceToBossLevel = Vector3.Distance(transform.position, bossBarrier.transform.position);
+            // Adjust the volume of the 'heartBeatAudio' based on the distance to the boss level.
+            // The volume incrweases as the player gets closer to the boss level.
             heartBeatAudio.volume = 1 - (distanceToBossLevel / initialDistanceToBossLevel);
         }
     }
 
-
+    /// <summary>
+    /// Called when a 2D collision occurs.
+    /// </summary>
+    /// <param name="collision">The collision data.</param>
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // Check if the collided object has the "portalTag" tag.
         if (collision.gameObject.tag == "portalTag")
         {
+            // Load the win scene. The player has won the game.
             SceneManager.LoadScene(3);
         }
         
     }
 
+    /// <summary>
+    /// Called when a 2D trigger collision occurs.
+    /// </summary>
+    /// <param name="collision">The trigger collision data.</param>
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // Check if the trigger collided object has the "waterBlockTag" tag.
         if (collision.gameObject.tag == "waterBlockTag")
         {
+            // Start the coroutine to handle the player character's death.
             StartCoroutine(Die());
         }
     }
